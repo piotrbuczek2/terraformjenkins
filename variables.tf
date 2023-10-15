f26ac5af3c7d38dc8696660828067191300b2c5f
piotrbuczek2 <97125424+piotrbuczek2@users.noreply.github.com>
2023-10-15 13:19:11 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,11 +2,3 @@ variable "aws_region" {
   description = "AWS region"
   default     = "us-west-1"
 }
-
-variable "aws_access_key" {
-  description = "access key"
-}
-
-variable "aws_secret_access_key" {
-  description = "secret access key"
-}
